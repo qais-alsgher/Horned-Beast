10a1b0b6e8cbc1019390dc83d52dfa105e3e27ed
qais-alsgher <qaisalsgher@gmail.com>
2022-07-28 03:01:04 +0300

    revole add favorite

--- src/component/mdalCar.js
+++ src/component/mdalCar.js
@@ -6,32 +6,54 @@ import Card from 'react-bootstrap/Card';
 import HornedBeast from './HornedBeast';
 
 class ModalCar extends React.Component{
+constructor(props){
+  super(props);
+  this.state={
+    show:false,
+    favorite:0
+  }
+}
+handleClose = () => {
+ this.props.handleClose()
+    
+}
+
+handleShow = () => {
+  this.props.handleShow()
+      }
+ 
+
+ handleClick=()=>{
+  this.props.handleClick()
+}
+
+
 
 render(){
 
 return(
 <>
-<Modal show={this.props.show} onHide={()=>this.props.handleClose}>
+<Modal show={this.props.show} onHide={this.handleClose}>
     <Modal.Header closeButton>
-      <Modal.Title>child</Modal.Title>
+      <Modal.Title>Horned HornedBeast</Modal.Title>
     </Modal.Header>
     <Modal.Body>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={this.props.url} />
+      <Card.Img variant="top" src={this.props.url} onClick={this.handleClick}/>
       <Card.Body>
         <Card.Title>{this.props.title}</Card.Title>
         <Card.Text>
         {this.props.desc}
         </Card.Text>
-        <span>{this.props.favorite}❤</span>
+        <span>{this.favorite}❤</span>
       </Card.Body>
     </Card>
     </Modal.Body>
     <Modal.Footer>
-      <Button variant="secondary" onClick={this.props.handleClose}>
+      <Button variant="secondary" onClick={this.handleClose}>
         Close
       </Button>
-      <Button variant="primary" onClick={this.props.handleClick}>
+      <Button variant="primary" onClick={this.handleClick}>
         Add to favorite
       </Button>
     </Modal.Footer>
@@ -41,6 +63,7 @@ return(
 
 )
 
-}}
+}
+}
 
 export default ModalCar;
